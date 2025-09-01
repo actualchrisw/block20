544a00a214dcdddc368d3f147d43ef51f702659d
Chris Warman <actualchrisw@gmail.com>
2025-09-01 13:28:22 -0600

    Add files via upload
    API functions started, Get functions created

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ const API = BASE + COHORT;
 let puppies = [];
 let selectedPuppy = null;
 
+// === API Functions ===
+
+// Fetch all puppies from the API
+async function getPuppies() {
+  try {
+    const response = await fetch(API + "/players"); // make request
+    const result = await response.json(); // convert response to JSON
+    puppies = result.data.players; // save list of puppies into state
+    render(); // render the UI
+  } catch (e) {
+    console.error(e); // log any errors
+  }
+}
+
+/** Get single puppy by ID */
+async function getPuppy(id) {
+  try {
+    const response = await fetch(API + "/players/" + id);
+    const result = await response.json();
+    selectedPuppy = result.data.player;
+    render();
+  } catch (e) {
+    console.error(e);
+  }
+}
