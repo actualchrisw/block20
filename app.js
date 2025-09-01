3f516079f6f572bed1896d9608c197f091248974
Chris Warman <actualchrisw@gmail.com>
2025-09-01 13:24:13 -0600

    Add files via upload
    State created

---
+++ app.js
@@ -0,0 +1,9 @@
+// === CONSTANTS ===
+const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
+const COHORT = "/2506-ct-web-pt"; 
+const API = BASE + COHORT;
+
+// === STATE ===
+let puppies = [];
+let selectedPuppy = null;
+
